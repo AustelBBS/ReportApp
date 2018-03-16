0df6639d6707672a0bdbd4236c5834b5f9e064ed
DonauMorgen <donaumorgen@macbook.local>
2018-03-15 18:18:36 -0600

    Initial Commit

---
+++ ReportApp/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ReportApp
+//
+//  Created by DonauMorgen on 13/03/18.
+//  Copyright © 2018 Los Ponis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mImageLogo: UIImageView!
+    @IBOutlet weak var leftConstraint: NSLayoutConstraint!
+    @IBOutlet weak var mConfig   : UIButton!
+    @IBOutlet weak var mComments : UIButton!
+    @IBOutlet weak var mReports  : UIButton!
+    var isMenuHidden = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mImageLogo.layer.cornerRadius = 8.0
+        mImageLogo.clipsToBounds = true
+        mConfig.layer.cornerRadius = 8.0
+        mComments.layer.cornerRadius = 8.0
+        mReports.layer.cornerRadius = 8.0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showMenu(_ sender: UIBarButtonItem) {
+        if isMenuHidden {
+            leftConstraint.constant = 0
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        } else {
+            leftConstraint.constant = -150
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        isMenuHidden = !isMenuHidden
+    }
+    
+    
+}
+
