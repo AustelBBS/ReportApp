0cd5a743696c548ecb33b8b0c178dd0c5910f3aa
Orlando Vergara <macintosh@macintoshs-mac-mini.local>
2018-10-25 12:38:46 -0500

    Fixed AutoLayout

---
+++ ReportApp/Service/CoreDataHandler.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataHandler.swift
+//  SimpleSQLITE
+//
+//  Created by Orlando Vergara @DonauMorgen on 21/03/18.
+//  Copyright © 2018 DonauMorgen. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataHandler: NSObject {
+
+    
+}
+
